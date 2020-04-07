e1984251ad381a576d9cb5d56b5088b94a948cdc
Irelia <yaokai729@outlook.com>
2020-04-07 12:44:25 +0800

    Updates

---
+++ precache-manifest.759f4a1fda8a4687802c87caee98370c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f61a54b1c1dfccb8db350e3e0e13e2b",
+    "url": "/potato/index.html"
+  },
+  {
+    "revision": "09721b359cdbe3424f86",
+    "url": "/potato/static/css/2.074e34cc.chunk.css"
+  },
+  {
+    "revision": "92e2c0cf482e86bcc46a",
+    "url": "/potato/static/css/main.7e33e1fc.chunk.css"
+  },
+  {
+    "revision": "09721b359cdbe3424f86",
+    "url": "/potato/static/js/2.0e8911c9.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/potato/static/js/2.0e8911c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e2c0cf482e86bcc46a",
+    "url": "/potato/static/js/main.42e9623f.chunk.js"
+  },
+  {
+    "revision": "07e4ff6ef93703cf43f7",
+    "url": "/potato/static/js/runtime-main.990e6979.js"
+  }
+]);
