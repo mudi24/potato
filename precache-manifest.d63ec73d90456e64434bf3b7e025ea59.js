574c7071bff0f70627301c3d36241e3005b5cd1e
Irelia <yaokai729@outlook.com>
2020-04-07 17:11:30 +0800

    Updates

---
+++ precache-manifest.d63ec73d90456e64434bf3b7e025ea59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c93b007aaf043c8789c17802e571060",
+    "url": "/potato/index.html"
+  },
+  {
+    "revision": "eee97aa1beda4f9df27e",
+    "url": "/potato/static/css/2.7eba4bce.chunk.css"
+  },
+  {
+    "revision": "02cf7bf8f18ae75ae9cf",
+    "url": "/potato/static/css/main.7e33e1fc.chunk.css"
+  },
+  {
+    "revision": "eee97aa1beda4f9df27e",
+    "url": "/potato/static/js/2.d2197b91.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/potato/static/js/2.d2197b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02cf7bf8f18ae75ae9cf",
+    "url": "/potato/static/js/main.cb78bd9f.chunk.js"
+  },
+  {
+    "revision": "07e4ff6ef93703cf43f7",
+    "url": "/potato/static/js/runtime-main.990e6979.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.88841ba15ff09599e0934abf8748603d.js"
+  "/potato/precache-manifest.d63ec73d90456e64434bf3b7e025ea59.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/potato/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
